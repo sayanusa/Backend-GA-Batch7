f2b25878963571e5c9d3abe6905a48a00de64010
sayanusa <hanifnusa@gmail.com>
2020-09-05 22:13:15 +0700

    revisi tugas

--- tugas DOM/js/index.js
+++ tugas DOM/js/index.js
@@ -1,62 +1,51 @@
-// var container = document.querySelector('.container');
-// console.log(container);
-
-// var input = document.querySelector('input');
-// console.log(input);
-
-// var taskData = document.querySelector('#task-data');
-// console.log(taskData);
-
-// var btn = document.getElementById('btn');
-// btn.addEventListener('click',submitHandler);
-
-//Task 1 : Prepare variable
-//Prepare global variable for data
-//Contains object
-//Example
 var tasks = [
     {
         id : 1,
         title : "Salary Income",
         type : "income",
-        total : 5000000
+        total : 5000000,
+    },
+    {
+        id : 2,
+        title : "Salary Expense",
+        type : "outcome",
+        total : 100000,
     }
 ];
 
 getData();
 
 function submitHandler(){
-    // console.log("Click already worked.");
+    //console.log("Click already worked.");
     var taskValue = document.getElementById('task-title').value;
-    var taskType = document.getElementById('task-type').value;
-    var taskTotal = document.getElementById('task-total').value;
-    
-    addData(taskValue,taskType,TaskTotal);
+    var taskStatus = document.getElementById('task-status').value;
+    var taskTotal= document.getElementById('task-total').value;
+    addData(taskValue,taskStatus,taskTotal);
     getData();
     document.getElementById('task-title').value = "";
-
+    document.getElementById('task-status').value = "";
+    document.getElementById('task-total').value = "";
     return false;
 }
-//Task 2 : getData
-//Code getData function to read the data from the global variable
+
 function getData(){
-    //Code here
     var taskData = document.getElementById('task-data');
     taskData.innerHTML = "";
     
     for(let i=0; i<tasks.length; i++){
         let taskHTML = `
-            <p>${tasks[i].id}. ${tasks[i].title} , type : ${tasks[i].type}, total : ${task[i].total}</p>
+            <p>${tasks[i].id}. ${tasks[i].title} , type : ${tasks[i].type} , total : ${tasks[i].total}</p>
         `
+        
         taskData.insertAdjacentHTML('beforeend',taskHTML);
     }
+    
 }
-
 function addData(title,type,total){
-    //Code here
     var taskObject = createObject(title,type,total);
     tasks.push(taskObject);
     console.log(`"${title}" has been inserted!`);
+
 }
 
 function createObject(title,type,total){
@@ -64,7 +53,7 @@ function createObject(title,type,total){
         id : generateId() + 1,
         title : title,
         type : type,
-        total : total,
+        total : total
     }
     return temp
 }
@@ -75,4 +64,3 @@ function generateId(){
     return id
     // return tasks[tasks.length - 1].id
 }
-
